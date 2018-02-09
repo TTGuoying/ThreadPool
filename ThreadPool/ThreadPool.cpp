7f72e32358a72ef23b0717220d7cb1863dc4d165
GuoyingTan <649188851@qq.com>
2018-02-09 20:02:32 +0800

    Add Project File
    Add Project File

---
+++ ThreadPool/ThreadPool.cpp
@@ -0,0 +1,164 @@
+#include "stdafx.h"
+#include "ThreadPool.h"
+
+
+ThreadPool::ThreadPool(size_t initSize, size_t maxSize) :
+	getTaskTask((TaskBase*)new GetTaskTask(this))
+{
+	this->maxSize = maxSize;
+	InitializeCriticalSection(&csThreadLock);
+	InitializeCriticalSection(&csWaitTaskLock);
+	stopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	completionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 1);
+
+	EnterCriticalSection(&csThreadLock);
+	threadList.clear();
+	for (size_t i = 0; i < initSize; i++)
+	{
+		Thread *thread = new Thread;
+		threadList.push_back(thread);
+	}
+	Thread *thread = new Thread;
+	thread->ExecuteTask(getTaskTask, NULL);
+	threadList.push_back(thread);
+	LeaveCriticalSection(&csThreadLock);
+}
+
+ThreadPool::~ThreadPool()
+{
+	SetEvent(stopEvent);
+	PostQueuedCompletionStatus(completionPort, 0, (DWORD)EXIT, NULL);
+	EnterCriticalSection(&csThreadLock);
+	list<Thread *>::iterator it = threadList.begin();
+	for ( ; it != threadList.end(); it++)
+	{
+		Thread *thread = *it;
+		delete thread;
+	}
+	threadList.clear();
+	LeaveCriticalSection(&csThreadLock);
+
+	
+	CloseHandle(stopEvent);
+	DeleteCriticalSection(&csThreadLock);
+	DeleteCriticalSection(&csWaitTaskLock);
+}
+
+BOOL ThreadPool::QueueTaskItem(shared_ptr<TaskBase> task, shared_ptr<TaskCallbackBase> taskCb)
+{
+	EnterCriticalSection(&csWaitTaskLock);
+	shared_ptr<WaitTask> waitTask(new WaitTask(task, taskCb));
+	waitTaskList.push(waitTask);
+	LeaveCriticalSection(&csWaitTaskLock);
+	PostQueuedCompletionStatus(completionPort, 0, (DWORD)GET_TASK, NULL);
+	return TRUE;
+}
+
+void ThreadPool::GetTaskExcute()
+{
+	Thread *thread = NULL;
+	shared_ptr<WaitTask> waitTask = NULL;
+
+	EnterCriticalSection(&csWaitTaskLock);
+	if (waitTaskList.size() > 0)
+	{
+		waitTask = waitTaskList.front();
+		waitTaskList.pop();
+	}
+	LeaveCriticalSection(&csWaitTaskLock);
+	if (waitTask == NULL)
+	{
+		return;
+	}
+
+	EnterCriticalSection(&csThreadLock);
+	list<Thread *>::iterator it = threadList.begin();
+	for (; it != threadList.end(); it++)
+	{
+		if (!(*it)->isBusy())
+		{
+			thread = *it;
+			break;
+		}
+	}
+	if (it == threadList.end() && getPoolSize() < maxSize)
+	{
+		thread = new Thread;
+		threadList.push_back(thread);
+	}
+	if (thread != NULL)
+	{
+		thread->ExecuteTask(waitTask->task, waitTask->taskCb);
+	}
+	LeaveCriticalSection(&csThreadLock);
+
+	if (thread == NULL)
+	{
+		EnterCriticalSection(&csWaitTaskLock);
+		waitTaskList.push(waitTask);
+		LeaveCriticalSection(&csWaitTaskLock);
+	}
+	
+}
+
+
+ThreadPool::Thread::Thread() :
+	busy(FALSE),
+	thread(INVALID_HANDLE_VALUE),
+	task(NULL),
+	taskCb(NULL),
+	exit(FALSE)
+{
+	thread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)ThreadProc, this, CREATE_SUSPENDED, 0);
+}
+
+ThreadPool::Thread::~Thread()
+{
+	exit = TRUE;
+	task = NULL;
+	taskCb = NULL;
+	ResumeThread(thread);
+	WaitForSingleObject(thread, INFINITE);
+	CloseHandle(thread);
+}
+
+BOOL ThreadPool::Thread::isBusy()
+{
+	return busy;
+}
+
+void ThreadPool::Thread::ExecuteTask(shared_ptr<TaskBase> t, shared_ptr<TaskCallbackBase> tcb)
+{
+	busy = TRUE;
+	task = t;
+	taskCb = tcb;
+	ResumeThread(thread);
+}
+
+DWORD ThreadPool::Thread::ThreadProc(LPARAM lParam)
+{
+	Thread *pThread = (Thread*)lParam;
+
+	while (true)
+	{
+		if (pThread->exit)
+			break; //Ïß³ÌÍË³ö
+
+		if (pThread->task == NULL && pThread->taskCb == NULL)
+		{
+			pThread->busy = FALSE;
+			SuspendThread(pThread->thread);
+			continue;
+		}
+
+		int resulst = pThread->task->Run();
+		if(pThread->taskCb)
+			pThread->taskCb->Run(resulst);
+		pThread->task = NULL;
+		pThread->taskCb == NULL;
+		pThread->busy = FALSE;
+		SuspendThread(pThread->thread);
+	}
+
+	return 0;
+}
---
+++ ThreadPool/ThreadPool.h
@@ -0,0 +1,147 @@
+/*
+==========================================================================
+* 类ThreadPool是本代码的核心类，类中自动维护线程池的创建和任务队列的派送
+
+* 其中的TaskBase类是封装了任务类
+* 其中的TaskBase类是封装了任务完成后的回调任务类
+
+*用法：定义一个ThreadPool变量，派生TaskBase类和TaskBase类，重载两个类中的Run()函数，然后调用ThreadPool的QueueTaskItem函()数即可
+
+Author: TTGuoying
+
+Date: 2018/02/09 19:55
+
+==========================================================================
+*/
+#pragma once
+#include <Windows.h>
+#include <list>
+#include <queue>
+#include <memory>
+
+using std::list;
+using std::queue;
+using std::shared_ptr;
+
+class TaskBase
+{
+public:
+	TaskBase() { }
+	virtual ~TaskBase() { }
+
+	WPARAM wParam;              // 参数1
+	LPARAM lParam;              // 参数2
+	virtual int Run() = 0;		// 任务函数
+
+};
+
+class TaskCallbackBase
+{
+public:
+	TaskCallbackBase() {}
+	virtual ~TaskCallbackBase() {}
+
+	virtual void Run(int result) = 0; // 回调函数
+
+};
+
+class ThreadPool
+{
+public:
+	ThreadPool(size_t initSize = 2, size_t maxSize = 100);
+	~ThreadPool();
+
+	BOOL QueueTaskItem(shared_ptr<TaskBase> task, shared_ptr<TaskCallbackBase> taskCb);
+	int getPoolSize() { return threadList.size() - 1; }
+
+private:
+	// 线程类(内部类)
+	class Thread
+	{
+	public:
+		Thread();
+		~Thread();
+
+		BOOL isBusy();									// 是否有任务在执行
+		void ExecuteTask(shared_ptr<TaskBase> t, shared_ptr<TaskCallbackBase> tcb);		// 执行任务
+
+	private:
+		BOOL	busy;									// 是否有任务在执行
+		BOOL    exit;									// 是否退出
+		HANDLE  thread;									// 线程句柄
+		shared_ptr<TaskBase>	task;					// 要执行的任务
+		shared_ptr<TaskCallbackBase> taskCb;			// 回调的任务
+		static DWORD WINAPI ThreadProc(LPARAM lParam);  // 线程函数
+	};
+
+	// IOCP的通知种类
+	enum WAIT_OPERATION_TYPE
+	{
+		GET_TASK,
+		EXIT
+	};
+
+	// 待执行的任务类
+	class WaitTask
+	{
+	public:
+		WaitTask(shared_ptr<TaskBase> task, shared_ptr<TaskCallbackBase> taskCb) 
+		{
+			this->task = task;
+			this->taskCb = taskCb;
+		}
+		~WaitTask() { task = NULL; taskCb = NULL; }
+
+		shared_ptr<TaskBase>	task;					// 要执行的任务
+		shared_ptr<TaskCallbackBase> taskCb;			// 回调的任务
+	};
+
+	// 从任务列表取任务的任务
+	class GetTaskTask : TaskBase
+	{
+	public:
+		GetTaskTask(ThreadPool *threadPool) { wParam = (WPARAM)threadPool; }
+		~GetTaskTask() { }
+
+		int Run()
+		{
+			ThreadPool *threadPool = (ThreadPool *)wParam;
+			BOOL bRet = FALSE;
+			DWORD dwBytes = 0;
+			WAIT_OPERATION_TYPE opType;
+			OVERLAPPED *ol;
+			while (WAIT_OBJECT_0 != WaitForSingleObject(threadPool->stopEvent, 0))
+			{
+				BOOL bRet = GetQueuedCompletionStatus(threadPool->completionPort, &dwBytes, (PULONG_PTR)&opType, &ol, INFINITE);
+				// 收到退出标志
+				if (EXIT == (DWORD)opType)
+				{
+					break;
+				}
+				else if (GET_TASK == (DWORD)opType)
+				{
+					threadPool->GetTaskExcute();
+				}
+			}
+
+			return 1;
+		}
+
+	};
+
+	void GetTaskExcute();
+
+	CRITICAL_SECTION csThreadLock;
+	list<Thread *> threadList;							// 线程列表
+
+	CRITICAL_SECTION csWaitTaskLock;
+	queue<shared_ptr<WaitTask>> waitTaskList;			// 任务列表
+
+	HANDLE					stopEvent;				// 通知线程退出的时间
+	HANDLE					completionPort;			// 完成端口
+	size_t maxSize;
+	shared_ptr<TaskBase>	getTaskTask;				// 取任务的任务
+};
+
+
+
